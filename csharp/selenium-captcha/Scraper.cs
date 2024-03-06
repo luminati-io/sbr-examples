d2eb4726fb7b7d3bc84ffac6536139aee905b4d3
Nikolai Sukharev <nikolai@brightdata.com>
2024-03-06 09:06:15 +0000

    add selenium-captcha for csharp

---
+++ csharp/selenium-captcha/Scraper.cs
@@ -0,0 +1,67 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+class Scraper
+{
+
+    private string _auth;
+
+    public Scraper(string auth)
+    {
+        _auth = auth;
+    }
+
+    private WebDriver Connect()
+    {
+        if (_auth == "USER:PASS")
+        {
+            throw new Exception("Provide Scraping Browsers credentials in AUTH"
+                    + " environment variable or update the script.");
+        }
+        var uri = new Uri($"https://{_auth}@brd.superproxy.io:9515");
+        var executor = new HttpCommandExecutor(uri, TimeSpan.FromSeconds(60));
+        var cdpCommand = new HttpCommandInfo(HttpCommandInfo.PostCommand,
+                "/session/{sessionId}/goog/cdp/execute");
+        executor.TryAddCommand("cdp", cdpCommand);
+        var capabilities = new ChromeOptions().ToCapabilities();
+        return new RemoteWebDriver(executor, capabilities);
+    }
+
+    public void Scrape(string url)
+    {
+        Log("Connecting to Browser...");
+        using var driver = Connect();
+        Log($"Connected! Navigating to {url}...");
+        driver.Navigate().GoToUrl(url);
+        Log("Navigated! Waiting captcha to detect and solve...");
+        var result = (Dictionary<string, object>) driver.ExecuteCustomDriverCommand("cdp", new ()
+        {
+            {"cmd", "Captcha.solve"},
+            {"params", new Dictionary<string, object>(){
+                {"detectTimeout", 10000},
+            }},
+        }) as Dictionary<string, object>;
+        var status = (string) result!["status"];
+        Log($"Captcha status: {status}");
+    }
+
+    private static string Env(string name, string defaultValue)
+    {
+        return Environment.GetEnvironmentVariable(name) ?? defaultValue;
+    }
+
+    private static void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static void Main()
+    {
+        var auth = Env("AUTH", "USER:PASS");
+        var url = Env("TARGET_URL", "https://example.com");
+        var scraper = new Scraper(auth);
+        scraper.Scrape(url);
+    }
+
+}
