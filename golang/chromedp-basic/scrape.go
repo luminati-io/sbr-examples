72ab08bb5fd98f9aa4e7edceb0d2c99168f479ca
Nikolai Sukharev <nikolai@brightdata.com>
2024-05-28 13:13:28 +0000

    Add logging for basic session

--- golang/chromedp-basic/scrape.go
+++ golang/chromedp-basic/scrape.go
@@ -12,8 +12,9 @@ import (
 	"github.com/gobwas/ws"
 )
 
-func connect(ctx context.Context, auth string) (context.Context, context.CancelFunc) {
-	if auth == "" {
+func connect(ctx context.Context) (context.Context, context.CancelFunc) {
+	auth, ok := os.LookupEnv("AUTH")
+	if !ok {
 		log.Fatal("Provide Scraping Browsers credentials in AUTH environment variable or update the script.")
 	}
 	// chromedp doesn't support passing authorization in url, so we just set default upgrade request headers for underlying ws library (gowbas/ws)
@@ -25,28 +26,37 @@ func connect(ctx context.Context, auth string) (context.Context, context.CancelF
 	return chromedp.NewRemoteAllocator(ctx, wsUrl, chromedp.NoModifyURL)
 }
 
+func action(f func()) chromedp.ActionFunc {
+	return chromedp.ActionFunc(func(ctx context.Context) error {
+		f()
+		return nil
+	})
+}
+
 func scrape(ctx context.Context, url string) (string, error) {
 	var result string
 	bCtx, bCtxCancel := chromedp.NewContext(ctx)
 	defer bCtxCancel()
+	fmt.Printf("Connecting to Scraping Browser...\n")
 	err := chromedp.Run(bCtx,
+		action(func() { fmt.Printf("Connected! Navigating to %s...\n", url) }),
 		chromedp.Navigate(url),
+		action(func() { fmt.Print("Navigated! Scraping page content...\n") }),
 		chromedp.EvaluateAsDevTools("document.documentElement.outerHTML", &result),
+		action(func() { fmt.Printf("Scraped! Data: %s\n", result) }),
 	)
 	return result, err
 }
 
 func main() {
-	auth := os.Getenv("AUTH")
-	url := os.Getenv("TARGET_URL")
-	if url == "" {
+	url, ok := os.LookupEnv("TARGET_URL")
+	if !ok {
 		url = "https://example.com"
 	}
-	ctx, ctxCancel := connect(context.Background(), auth)
+	ctx, ctxCancel := connect(context.Background())
 	defer ctxCancel()
-	result, err := scrape(ctx, url)
+	_, err := scrape(ctx, url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Result: %s\n", result)
 }
