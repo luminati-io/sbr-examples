117775f8b2d4944f67a2da55e9fb57736b34ddf9
Nikolai Sukharev <nikolai@brightdata.com>
2024-08-22 23:03:16 +0000

    use Download api for file download example

--- nodejs/puppeteer-file-download/scrape.js
+++ nodejs/puppeteer-file-download/scrape.js
@@ -17,51 +17,28 @@ async function scrape(url = TARGET_URL, selector = SELECTOR, filename = FILENAME
     const browserWSEndpoint = `wss://${AUTH}@brd.superproxy.io:9222`;
     const browser = await puppeteer.connect({ browserWSEndpoint });
     try {
-        console.log(`Connected! Navigating to ${url}...`);
+        console.log(`Connected! Enable file download...`);
         const page = await browser.newPage();
         const client = await page.createCDPSession();
+        await client.send('Download.enable', { allowedContentTypes: ['application/zip'] });
+        console.log(`Navigating to ${url}...`);
         await page.goto(url, { timeout: 2 * 60 * 1000 });
         console.log(`Navigated! Initiating download...`);
-        const requestId = await initiateDownload(page, client, selector);
-        console.log(`Download started! Stream it to ${filename}...`);
-        const { stream } = await client.send('Fetch.takeResponseBodyAsStream', {
-            requestId,
-        });
+        await Promise.all([
+            new Promise(resolve => client.once('Download.downloadRequest', resolve)),
+            page.click(selector),
+        ]);
+        console.log(`Download completed! Saving it to ${filename}...`);
+        const { id } = await client.send('Download.getLastCompleted');
+        const { body, base64Encoded } = await client.send('Download.getDownloadedBody', { id });
+        const bytes = Buffer.from(body, base64Encoded ? 'base64' : 'utf8');
         const file = await fs.open(filename, 'w');
-        let fileSize = 0;
-        while (true) {
-            const { data, base64Encoded, eof } = await client.send('IO.read', {
-                handle: stream,
-            });
-            const chunk = Buffer.from(data, base64Encoded ? 'base64' : 'utf8');
-            await file.write(chunk);
-            fileSize += chunk.byteLength;
-            if (eof) {
-                break;
-            }
-        }
+        await file.write(bytes);
         await file.close();
-        console.log(`Download saved! Size: ${fileSize}.`);
+        console.log(`Download saved! Size: ${bytes.length}.`);
     } finally {
         await browser.close();
     }
-}
-
-async function initiateDownload(page, client, selector) {
-    await client.send('Fetch.enable', {
-        patterns: [{
-            requestStage: 'Response',
-            resourceType: 'Document',
-        }],
-    });
-    return await new Promise((resolve, reject) => {
-        client.on('Fetch.requestPaused', ({ requestId }) => {
-            resolve(requestId);
-        });
-        page.click(selector).catch(error => {
-            reject(error);
-        });
-    });
 }
 
 function getErrorDetails(error) {
