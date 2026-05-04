f13d9cbfe5cd039ad70ef5082ab9a018db645bb5
nikolai <nikolai@brightdata.com>
2026-05-04 19:36:09 +0700

    Split download in chunks

--- nodejs/puppeteer-file-download/scrape.js
+++ nodejs/puppeteer-file-download/scrape.js
@@ -6,6 +6,7 @@ const {
     TARGET_URL = 'https://calmcode.io/datasets/bigmac',
     SELECTOR = 'button.border',
     FILENAME = './testfile.csv',
+    CHUNK_SIZE = '1048576',
 } = process.env;
 
 async function scrape(url = TARGET_URL, selector = SELECTOR, filename = FILENAME) {
@@ -30,14 +31,29 @@ async function scrape(url = TARGET_URL, selector = SELECTOR, filename = FILENAME
         ]);
         console.log(`Download completed! Saving it to ${filename}...`);
         const { id } = await client.send('Download.getLastCompleted');
-        const { body, base64Encoded } = await client.send('Download.getDownloadedBody', { id });
-        const bytes = Buffer.from(body, base64Encoded ? 'base64' : 'utf8');
+        const body = await getDownloadedBody(client, id);
         const file = await fs.open(filename, 'w');
-        await file.write(bytes);
+        await file.write(body);
         await file.close();
-        console.log(`Download saved! Size: ${bytes.length}.`);
+        console.log(`Download saved! Size: ${body.length}.`);
     } finally {
         await browser.close();
+    }
+}
+
+async function getDownloadedBody(client, id, chunkSize = +CHUNK_SIZE){
+    let offset = 0;
+    const parts = [];
+    for (let i = 1;; i++) {
+        const { body, base64Encoded, eof } =
+            await client.send('Download.getDownloadedBody', { id, offset, size: chunkSize });
+        const chunk = Buffer.from(body, base64Encoded ? 'base64' : 'utf8');
+        console.log(`Download chunk #${i}! Size: ${chunk.length}`);
+        parts.push(chunk);
+        offset += chunk.length;
+        if (eof) {
+            return Buffer.concat(parts)
+        }
     }
 }
 
